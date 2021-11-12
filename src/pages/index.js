de940ce291847d05dea1790d73ae6859caa5bf7c
l33th <l33th@protonmail.ch>
2021-11-12 17:45:42 +0200

    landing page logo styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Layout from '../components/layout'
+import '../components/layout.css'
 import { Helmet } from 'react-helmet'
 import { FaCoffee } from 'react-icons/fa'
+import { Link } from 'gatsby'
+import logo from '../images/logo.svg'
 
 export default function Home() {
   return (
     <Layout>
+      <Link to="/" className="navbar-brand">
+        <img className="logo-landing" src={logo} alt="logo-landing" />
+      </Link>
       <h2>Welcome to Jittery!</h2>
       <h6>A shot of jitters to feed your focus.</h6>
       <div className="application">

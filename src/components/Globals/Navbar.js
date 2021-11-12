53f824823f12cff5c58f1c410f5a0b38f085ba8e
l33th <l33th@protonmail.ch>
2021-11-12 18:20:38 +0200

    logo updates

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import logo from '../../images/logo.svg'
+import logo from '../../images/logo.png'
 import { FaCartArrowDown } from 'react-icons/fa'
 
 export default class Navbar extends React.Component {

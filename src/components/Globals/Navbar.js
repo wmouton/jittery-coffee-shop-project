af93c3123de5ce66319072f451aae4ea867c7b89
l33th <l33th@protonmail.ch>
2021-11-12 16:54:42 +0200

    display home and about links

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -3,34 +3,47 @@ import { Link } from 'gatsby'
 import logo from '../../images/logo.svg'
 import { FaCartArrowDown } from 'react-icons/fa'
 
-const obj = [
-  { id: 1, path: '/', text: 'home' },
-  { id: 2, path: '/about', text: 'about' },
-]
+export default class Navbar extends React.Component {
+  state = {
+    navbarOpen: false,
+    css: 'collapse navbar-collapse',
+    links: [
+      { id: 1, path: '/', text: 'home' },
+      { id: 2, path: '/about', text: 'about' },
+    ],
+  }
 
-const Navbar = () => {
-  const [navbarOpen, setNavbarOpen] = useState(false)
-  const [css, setCss] = useState('collapse navbar-collapse')
-  const [links, setLinks] = useState(obj)
-
-  const navbarHandler = () => {
+  navbarHandler = () => {
     console.log('hello')
   }
-  return (
-    <nav className="navbar navbar-expand-sm bg-light navbar-light">
-      <Link to="/" className="navbar-brand">
-        <img className="logo" src={logo} alt="logo" />
-      </Link>
-      <h1>Navbar Content</h1>
-      <button
-        className="navbar-toggler"
-        type="button"
-        onClick={() => navbarHandler}
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-    </nav>
-  )
-}
 
-export default Navbar
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+        <Link to="/" className="navbar-brand">
+          <img className="logo" src={logo} alt="logo" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={this.navbarHandler}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {this.state.links.map(link => {
+              return (
+                <li key={link.id} className="nav-item">
+                  <Link to={link.path} className="nav-link text-capitalize">
+                    {link.text}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}

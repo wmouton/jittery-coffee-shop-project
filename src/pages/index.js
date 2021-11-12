9926d16b732ab76bbce01d8c8feda30a4cbb33b3
l33th <l33th@protonmail.ch>
2021-11-12 18:29:26 +0200

    removed coffee icon for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import '../components/layout.css'
 import { Helmet } from 'react-helmet'
-import { FaCoffee } from 'react-icons/fa'
+// import { FaCoffee } from 'react-icons/fa'
 import { Link } from 'gatsby'
 import logo from '../images/jittery-logo.png'
 

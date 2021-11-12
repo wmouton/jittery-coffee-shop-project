44369fe2fca80c2c921279730879f80040628fe3
l33th <l33th@protonmail.ch>
2021-11-12 15:30:09 +0200

    navbar logo setup

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'gatsby'
+import logo from '../../images/logo.svg'
+import { FaCartArrowDown } from 'react-icons/fa'
+
+const obj = [
+  { id: 1, path: '/', text: 'home' },
+  { id: 2, path: '/about', text: 'about' },
+]
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false)
+  const [css, setCss] = useState('collapse navbar-collapse')
+  const [links, setLinks] = useState(obj)
+
+  const navbarHandler = () => {
+    console.log('hello')
+  }
   return (
-    <div>
+    <nav className="navbar navbar-expand-sm bg-light navbar-light">
+      <Link to="/" className="navbar-brand">
+        <img className="logo" src={logo} alt="logo" />
+      </Link>
       <h1>Navbar Content</h1>
-    </div>
+    </nav>
   )
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './layout.css'
 import './bootstrap.min.css'
 import Navbar from './Globals/Navbar'
 

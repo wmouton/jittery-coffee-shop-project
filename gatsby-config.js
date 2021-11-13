aeabffb27228c918f5e77d15c5687d6b8f5cc84e
l33th <l33th@protonmail.ch>
2021-11-13 12:48:53 +0200

    added background section and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,8 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+const path = require(`path`)
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -11,5 +13,17 @@ module.exports = {
     description: `Feed your focus with our selection of delicious coffee.`,
     author: `wmouton`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        // path: `${__dirname}/src/images}`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,15 @@ import React from 'react'
 import Layout from '../components/layout'
 import '../components/layout.css'
 import { Helmet } from 'react-helmet'
+import {graphql} from 'gatsby'
 // import { FaCoffee } from 'react-icons/fa'
-import { Link } from 'gatsby'
 import logo from '../images/jittery-logo.png'
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <Layout>
-      <Link to="/" className="navbar-brand">
-        <img className="logo-landing" src={logo} alt="logo-landing" />
-      </Link>
-      <h2>Welcome to Jittery!</h2>
+      <img className="logo-landing" src={logo} alt="logo-landing" />
+      <h3>Welcome to Jittery!</h3>
       <h6>A shot of jitters to feed your focus.</h6>
       <div className="application">
         <Helmet>
@@ -24,3 +22,15 @@ export default function Home() {
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childrenImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`

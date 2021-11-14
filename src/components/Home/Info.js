e162ce89ef054ea9e0fc856ff207606af283ab25
l33th <l33th@protonmail.ch>
2021-11-14 06:26:14 +0200

    added button and button styles

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -10,6 +10,9 @@ const Info = () => {
             <p className="lead text-muted mb-5">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, aspernatur vitae enim unde deleniti cupiditate, debitis molestiae doloribus accusantium, voluptatibus id hic aliquam! Cumque voluptas alias natus culpa fuga laudantium mollitia pariatur corrupti quam explicabo placeat, ipsam nesciunt amet fugit inventore nostrum perspiciatis. Ipsam, saepe accusamus, totam officiis distinctio autem tempore asperiores alias quia quibusdam aspernatur. Amet facere maiores velit modi ullam minima iusto repellendus beatae, aperiam id consectetur! Provident facere maiores amet non, vero eius sunt minus ratione voluptatem!
             </p>
+            <Link to='/about/' >
+              <button className='btn text-uppercase btn-yellow'>about</button>
+            </Link>
           </div>
         </div>
       </div>
